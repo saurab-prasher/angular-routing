712e83d1363bc8e865b2898ea05f9e61e0bb32e2
saurabprasher <saurab015@gmail.com>
2024-07-26 14:02:17 -0400

    nested routes added

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,11 +2,23 @@ import { Routes } from '@angular/router';
 import { TasksComponent } from './app/tasks/tasks.component';
 import { NoTaskComponent } from './app/tasks/no-task/no-task.component';
 import { UserTasksComponent } from './app/users/user-tasks/user-tasks.component';
+import { NewTaskComponent } from './app/tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   { path: '', component: NoTaskComponent },
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks', // <your-domain>/users/:userId/tasks
+        component: TasksComponent,
+      },
+
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -2,34 +2,50 @@ import {
   Component,
   Input,
   OnChanges,
+  OnDestroy,
   OnInit,
   SimpleChanges,
 } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterLink, RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent implements OnInit, OnChanges {
-  @Input({ required: true }) userId!: string;
+export class UserTasksComponent implements OnInit, OnChanges, OnDestroy {
+  // @Input({ required: true }) userId!: string;
   userName?: string;
+  subscription!: Subscription;
 
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.loadUserName();
+    this.subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) =>
+        (this.userName =
+          this.userService.users.find((u) => u.id === paramMap.get('userId'))
+            ?.name || ''),
+    });
+    // this.loadUserName();
   }
 
   ngOnChanges() {
-    this.loadUserName();
+    // this.loadUserName();
   }
-
-  loadUserName() {
-    this.userName = this.userService.users.find(
-      (user) => user.id === this.userId
-    )?.name;
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
+  // loadUserName() {
+  //   this.userName = this.userService.users.find(
+  //     (user) => user.id === this.userId
+  //   )?.name;
+  // }
 }
